97a2be04a02650ac3280d0f8932724c129c229e0
nikhitkumar <nikhitkumar00@gmail.com>
2023-04-11 09:50:43 +0530

    updated code

--- stop-and-wait-socket-programming/server.c
+++ stop-and-wait-socket-programming/server.c
@@ -1,69 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#define PACKET_SIZE 1024
-#define WINDOW_SIZE 1
 
-typedef struct {
-    int seq_num;
-    char data[PACKET_SIZE];
-} packet;
+typedef struct packet{
+    char data[1024];
+}Packet;
 
-int main() {
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    socklen_t addr_len = sizeof(serv_addr);
-    packet pkt;
-    int seq_num = 0;
-    int total_packets = 10;
+typedef struct frame{
+    int frame_kind; //ACK:0, SEQ:1 FIN:2
+    int sq_no;
+    int ack;
+    Packet packet;
+}Frame;
 
-    // Create socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sockfd < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char** argv){
 
-    // Set server address
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	if (argc != 2){
+		printf("Usage: %s <port>", argv[0]);
+		exit(0);	
+	}
 
-    // Send packets
-    while (seq_num < total_packets) {
-        // Create packet
-        pkt.seq_num = seq_num;
-        sprintf(pkt.data, "Packet %d", seq_num);
+	int port = atoi(argv[1]);
+	int sockfd;
+	struct sockaddr_in serverAddr, newAddr;
+	char buffer[1024];
+	socklen_t addr_size;
 
-        // Send packet
-        if (sendto(sockfd, &pkt, sizeof(pkt), 0, (struct sockaddr*) &serv_addr, addr_len) < 0) {
-            perror("sendto failed");
-            exit(EXIT_FAILURE);
-        }
+	int frame_id=0;
+	Frame frame_recv;
+	Frame frame_send;	
 
-        printf("Sent packet %d\n", seq_num);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-        // Wait for ACK
-        packet ack;
-        if (recvfrom(sockfd, &ack, sizeof(ack), 0, (struct sockaddr*) &serv_addr, &addr_len) < 0) {
-            perror("recvfrom failed");
-            exit(EXIT_FAILURE);
-        }
+	bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	addr_size = sizeof(newAddr);
 
-        if (ack.seq_num == seq_num) {
-            printf("Received ACK for packet %d\n", seq_num);
-            seq_num++;
-        }
-    }
-
-    // Close socket
-    close(sockfd);
-
-    return 0;
+	while(1){
+		int f_recv_size = recvfrom(sockfd, &frame_recv, sizeof(Frame), 0, (struct sockaddr*)&newAddr, &addr_size);
+		if (f_recv_size > 0 && frame_recv.frame_kind == 1 && frame_recv.sq_no == frame_id){
+			printf("[+]Frame Received: %s\n", frame_recv.packet.data);
+			
+			frame_send.sq_no = 0;
+			frame_send.frame_kind = 0;
+			frame_send.ack = frame_recv.sq_no + 1;
+			sendto(sockfd, &frame_send, sizeof(frame_send), 0, (struct sockaddr*)&newAddr, addr_size);
+			printf("[+]Ack Send\n");
+		}else{
+			printf("[+]Frame Not Received\n");
+		}
+		frame_id++;	
+	}
+	
+	close(sockfd);
+	return 0;
 }
-
