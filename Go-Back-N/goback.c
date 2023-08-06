394d060be2591f8ba6058e866c3f7e0cc46ba6e7
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-07 00:31:49 +0530

    added go back n

--- Go-Back-N/goback.c
+++ Go-Back-N/goback.c
@@ -20,19 +20,22 @@ void main()
     {
         for (int i = base; i < base + WINDOWSIZE; i++)
         {
-            if (ackrecieved[i] == false)
-            {
-                printf("\n\t\t\t\t\t\t[%d][%d][%d][%d]", base, base + 1, base + 2, base + 3);
-                printf("\nFrame[%d] sent", i);
-                sleep(3);
 
-                srand(time(NULL));
-                if (rand() % 2 == 0)
-                {
-                    printf("\n\t\t\tAcknowledge[%d] recieved", i);
-                    ackrecieved[i] = true;
-                }
+            printf("\n\t\t\t\t\t\t[%d][%d][%d][%d]", base, base + 1, base + 2, base + 3);
+            printf("\nFrame[%d] sent", i);
+            sleep(1);
+
+            srand(time(NULL));
+            if (rand() % 2 == 0 && ackexpected == i)
+            {
+                printf("\n\t\t\tAcknowledge[%d] recieved", i);
+                ackrecieved[i] = true;
             }
+            else
+            {
+                ackrecieved[i] = false;
+            }
+
             if (ackrecieved[ackexpected] == true)
             {
                 base++;
---
+++ Selective Repeat/selectiverepeat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define WINDOWSIZE 4
+
+void main()
+{
+    int base = 0, ackexpected = 0;
+    bool ackrecieved[999];
+
+    for (int i = 0; i < 999; i++)
+    {
+        ackrecieved[i] = false;
+    }
+
+    while (1)
+    {
+        for (int i = base; i < base + WINDOWSIZE; i++)
+        {
+            if (ackrecieved[i] == false)
+            {
+                printf("\n\t\t\t\t\t\t[%d][%d][%d][%d]", base, base + 1, base + 2, base + 3);
+                printf("\nFrame[%d] sent", i);
+                sleep(3);
+
+                srand(time(NULL));
+                if (rand() % 2 == 0)
+                {
+                    printf("\n\t\t\tAcknowledge[%d] recieved", i);
+                    ackrecieved[i] = true;
+                }
+                else
+                {
+                    printf("\n\t\t\tNegative Acknowledge[%d] sent", i);
+                }
+            }
+            if (ackrecieved[ackexpected] == true)
+            {
+                base++;
+                ackexpected++;
+            }
+        }
+    }
+}
