0efac765db77801f9360a751ee7f0dddebe91b35
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-07 12:47:57 +0530

    linkstate added

--- Link State Routung/linkstate.c
+++ Link State Routung/linkstate.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int count, src_router, i, j, k,
-        w,
-        v, min;
+    int count, src_router, i, j, k, w, v, min;
     int cost_matrix[100][100], dist[100], last[100];
     int flag[100];
     printf("Enter the no. of routers: ");
