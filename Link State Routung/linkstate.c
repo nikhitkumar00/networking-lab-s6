215f0b44f33f5dea23bccd0d7c9d160258386b04
nikhitkumar <nikhitkumar00@gmail.com>
2023-08-07 12:59:54 +0530

    updated link state

--- Link State Routung/linkstate.c
+++ Link State Routung/linkstate.c
@@ -1,63 +1,65 @@
 #include <stdio.h>
-int main()
-{
-    int count, src_router, i, j, k, w, v, min;
-    int cost_matrix[100][100], dist[100], last[100];
-    int flag[100];
-    printf("Enter the no. of routers: ");
-    scanf("%d", &count);
+
+int main() {
+    int routerCount, sourceRouter, i, j, k, w, v, min;
+
+    printf("Enter the number of routers: ");
+    scanf("%d", &routerCount);
+
+    int costMatrix[100][100], distance[100], previousRouter[100], visited[100];
+
     printf("Enter the cost matrix values:\n");
-    for (i = 0; i < count; i++)
-    {
-        for (j = 0; j < count; j++)
-        {
-            printf("%d->%d:", i, j);
-            scanf("%d", &cost_matrix[i][j]);
-            if (cost_matrix[i][j] < 0)
-                cost_matrix[i][j] = 1000;
+    for (i = 0; i < routerCount; i++) {
+        for (j = 0; j < routerCount; j++) {
+            printf("%d -> %d: ", i, j);
+            scanf("%d", &costMatrix[i][j]);
+
+            // Treat negative values as infinity
+            if (costMatrix[i][j] < 0) {
+                costMatrix[i][j] = 1000;
+            }
         }
     }
+
     printf("Enter the source router: ");
-    scanf("%d", &src_router);
-    for (v = 0; v < count; v++)
-    {
-        flag[v] = 0;
-        last[v] = src_router;
-        dist[v] = cost_matrix[src_router][v];
+    scanf("%d", &sourceRouter);
+
+    // Initialize arrays
+    for (v = 0; v < routerCount; v++) {
+        visited[v] = 0;
+        previousRouter[v] = sourceRouter;
+        distance[v] = costMatrix[sourceRouter][v];
     }
-    flag[src_router] = 1;
-    for (i = 0; i < count; i++)
-    {
+    visited[sourceRouter] = 1;
+
+    // Dijkstra's algorithm
+    for (i = 0; i < routerCount; i++) {
         min = 1000;
-        for (w = 0; w < count; w++)
-        {
-            if (!flag[w])
-                if (dist[w] < min)
-                {
-                    v = w;
-                    min = dist[w];
-                }
+        for (w = 0; w < routerCount; w++) {
+            if (!visited[w] && distance[w] < min) {
+                v = w;
+                min = distance[w];
+            }
         }
-        flag[v] = 1;
-        for (w = 0; w < count; w++)
-        {
-            if (!flag[w])
-                if (min + cost_matrix[v][w] < dist[w])
-                {
-                    dist[w] = min + cost_matrix[v][w];
-                    last[w] = v;
-                }
+        visited[v] = 1;
+        for (w = 0; w < routerCount; w++) {
+            if (!visited[w] && min + costMatrix[v][w] < distance[w]) {
+                distance[w] = min + costMatrix[v][w];
+                previousRouter[w] = v;
+            }
         }
     }
-    for (i = 0; i < count; i++)
-    {
-        printf("\n%d==>%d: Path taken:%d", src_router, i, i);
+
+    // Print results
+    for (i = 0; i < routerCount; i++) {
+        printf("\n%d => %d: Path taken: %d", sourceRouter, i, i);
         w = i;
-        while (w != src_router)
-        {
-            printf("<--%d", last[w]);
-            w = last[w];
+        while (w != sourceRouter) {
+            printf(" <- %d", previousRouter[w]);
+            w = previousRouter[w];
         }
-        printf("\nShortest path cost: %d\n", dist[i]);
+        printf("\nShortest path cost: %d\n", distance[i]);
     }
+
+    return 0;
 }
